43140bf38bbe365ff90cf294c69ca94f5df05fc3
meet49 <meetsojitra49@gmail.com>
2024-03-06 14:46:25 +0530

    counter using redux

---
+++ counterredux/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Counter from './features/counter/Counter'
+
+const App = () => {
+  return (
+    <div>
+      <Counter/>
+    </div>
+  )
+}
+
+export default App
