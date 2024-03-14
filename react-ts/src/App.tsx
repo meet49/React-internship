7a366266267d37d259caca2f80170823b180f579
meet49 <meetsojitra49@gmail.com>
2024-03-14 17:59:15 +0530

    start teh ts

---
+++ react-ts/src/App.tsx
@@ -0,0 +1,11 @@
+import Todo from "./componets/Todo"
+
+const App = () => {
+  return (
+    <div>
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
---
+++ react-ts/src/componets/Todo.tsx
@@ -0,0 +1,11 @@
+
+function Todo() {
+  return (
+    <ul>
+      <li>hello</li>
+      <li>world</li>
+    </ul>
+  )
+}
+
+export default Todo
